737896e40fb01082c1daa79930ee5be97757d835
Victor Augusto de Almeida Silva <victoraugustoas41@gmail.com>
2022-09-05 17:06:11 -0300

    Finalizando o IMC. Utilizando textinputmask no projeto

--- calculoImc/App.js
+++ calculoImc/App.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { TextInputMask } from 'react-native-masked-text'
 
 export default function App() {
 
   const [ peso, setPeso ] = React.useState();
   const [ altura, setAltura ] = React.useState();
   const [ res, setRes ] = React.useState();
+  const [ cor, setCor ] = React.useState("white");
 
   function calcular() {
-    let imc = peso / (altura * altura);
+    const p = peso.replace(".", "").replace(",", "."); // 85,50 => 85.50
+    const a = altura.replace(".", "").replace(",", "."); // 1,70  => 1.70
 
-    setRes(imc)
-  }
 
+    let imc = p / (a * a);
+
+      if(imc < 20 ){
+        setRes( `${imc.toFixed(2)} (Seu peso está abaixo da faixa considerada normal)`)
+        setCor('red')
+      } 
+        else if( imc >= 20 && imc < 25){
+          setRes(`${imc.toFixed(2)} (Seu peso está dentro da faixa considerada normal)`)
+          setCor('green')
+      }
+        else if(imc >= 25 && imc < 30){
+          setRes(`${imc.toFixed(2)} (Você está na faixa chamada de “excesso de peso")`)
+          setCor('yellow')
+        }
+        else if(imc >= 30 && imc < 35){
+          setRes(`${imc.toFixed(2)} (Você está na faixa chamada de obesidade leve)`)
+          setCor('orange')
+        }
+        else if(imc >= 35 && imc <40){
+          setRes(`${imc.toFixed(2)} (Você está na faixa chamada de obesidade moderada)`)
+          setCor('purple')
+        }
+        else if(imc >= 40){
+          setRes(`${imc.toFixed(2)} (Você está na faixa chamada de obesidade mórbida)`)
+          setCor('red')
+        }
+      }
   return (
     <SafeAreaView style={styles.container}>
       <View>
@@ -20,17 +48,29 @@ export default function App() {
       </View>
       <View style={styles.textoPeso}>
         <Text style={styles.texto}>Peso: </Text>
-        <TextInput style={styles.input} placeholder={'Digite seu peso'} onChangeText={setPeso} value={peso}></TextInput>
+        <TextInputMask type={'money'} options={{
+              precision: 2,
+              separator: ',',
+              delimiter: '',
+              unit: '',
+              suffixUnit: ''
+            }} style={styles.input} placeholder="Digite o valor" value={peso} onChangeText={setPeso} />
       </View>
       <View style={styles.textoPeso}>
         <Text style={styles.texto}>Altura: </Text>
-        <TextInput style={styles.input} placeholder={'Digite sua Altura'} onChangeText={setAltura} value={altura}></TextInput>
+        <TextInputMask type={'money'} options={{
+              precision: 2,
+              separator: ',',
+              delimiter: '',
+              unit: '',
+              suffixUnit: ''
+            }} style={styles.input} placeholder="Digite o valor" value={altura} onChangeText={setAltura} />
       </View>
       <View>
         <Button title='Calcular' onPress={() => calcular()}></Button>
       </View>
       <View>
-        <Text>IMC: {res}</Text>
+        <Text style={[styles.textResult, { backgroundColor: cor, padding: 5}]}>IMC: {res}</Text>
       </View>
     </SafeAreaView>
   );
@@ -56,6 +96,9 @@ const styles = StyleSheet.create({
     paddingTop: 70,
   },
   texto:{
+    fontSize: 20,
+  },
+  textResult:{
     fontSize: 20,
   },
 });
