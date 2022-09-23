6165330801f98bdb15ea3dbc454ecfcd5e47a377
Victor Augusto de Almeida Silva <victoraugustoas41@gmail.com>
2022-09-23 15:05:29 -0300

    atualizando aplicações

---
+++ NavRoots/App.js
@@ -0,0 +1,15 @@
+import Pagina from './src/components/Pagina'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Pagina nome='Pagina 1' />
+      <Pagina nome='Pagina 2' />
+    </View>
+
+  )
+
+}
+
+
---
+++ planilhaPropsComponent/App.js
@@ -0,0 +1,8 @@
+import PlanilhaPage from "./src/pages/PlanilhaPage";
+
+export default function App() {
+  return (
+    <PlanilhaPage />
+  );
+}
+
